bfd62f96d3894c353ee7cc9472325f6a2c9dbcf6
gazizdev <gaziz@java.kz>
2026-05-12 00:20:59 +0500

    init

--- src/main/java/kz/cyber/acf/config/CorsConfig.java
+++ src/main/java/kz/cyber/acf/config/CorsConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -14,10 +15,14 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @WebFilter("/*")
 public class CorsConfig implements Filter {
+
+    @Value("${application.url}")
+    private String allowedOrigin;
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
